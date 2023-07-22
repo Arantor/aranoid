87d4094ca4fb64ce95395344b270147f4d489112
Arantor <sleeping@myperch.org>
2023-07-22 20:22:59 +0100

    Add a power bar for the shield.

---
+++ Scripts/Power.gd
@@ -0,0 +1,30 @@
+extends Node
+
+@export var bar = 0.0
+@export var visible = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	visible = false
+
+func show(pos, bar_value):
+	$PowerContainer.position = pos
+	set_value(bar_value)
+
+func set_value(bar_value):
+	bar = bar_value
+	if (bar > 0):
+		visible = true
+	else:
+		hide()
+
+func hide():
+	visible = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$PowerContainer.visible = visible
+	if visible:
+		var width = ceil(bar * 11) * 4 + 1
+		$PowerContainer/Power.position = Vector2(floor(width / 2), 10)
+		$PowerContainer/Power.set_region_rect(Rect2(0, 0, width, 19))
--- Scripts/Shield.gd
+++ Scripts/Shield.gd
@@ -1,18 +1,33 @@
 extends Node2D
 
+var powerbar_node
+var powerbar
+var lifetimerdefault = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position = Vector2(12, 162)
-
+	powerbar_node = preload("res://Entities/Power.tscn")
+	powerbar = powerbar_node.instantiate()
+	var powerupeffects = get_node('../../PowerupEffects')
+	powerupeffects.add_child(powerbar)
+	powerbar.show(Vector2(267, 25), 1.0)
+	lifetimerdefault = $LifeTimer.wait_time
 
 func _process(_delta):
 	if $FadeOutTimer.is_stopped():
 		$Shield.modulate = Color(1, 1, 1, 1 - $FadeInTimer.time_left)
+		if $LifeTimer.is_stopped():
+			powerbar.set_value(1.0)
+		else:
+			powerbar.set_value($LifeTimer.time_left / lifetimerdefault)
 	else:
 		$Shield.modulate = Color(1, 1, 1, $FadeOutTimer.time_left)
+		powerbar.hide()
 
 
 func _on_fade_out_timer_timeout():
+	powerbar.queue_free()
 	queue_free()
 
 # Once the life timer has expired, fade out and kill the shield.
