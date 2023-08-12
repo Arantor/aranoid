ece8b0602b3c033847a80a48ec74ffaf79365dbb
Arantor <sleeping@myperch.org>
2023-08-13 00:15:52 +0100

    Add sound effect for multiball being triggered.

--- Scripts/PowerupMultiball.gd
+++ Scripts/PowerupMultiball.gd
@@ -8,6 +8,10 @@ func _physics_process(delta):
 		for ball in balls:
 			if ball.has_method("multi_split"):
 				ball.multi_split(3)
+
+		var sound_effect = get_node('../../Sounds/MultiballSound')
+		if not sound_effect.playing:
+			sound_effect.play()
 		queue_free()
 	
 	if position.y > 200:
